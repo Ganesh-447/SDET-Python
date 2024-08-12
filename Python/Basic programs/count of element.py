c58cbdae489322348e8fff91da2016c910df4462
Ganesh-447 <grandhiganesh47@gmail.com>
2024-08-12 23:38:29 +0530

    added count

--- Python/Basic programs/count of element.py
+++ Python/Basic programs/count of element.py
@@ -10,16 +10,25 @@
 # dic_1 ={char:count for char,count in repeated_words.items() if count>1 }
 # print(dic_1)
 
-a= 'ganesh grandhi'
-key_value ={}
-for char in a:
-    if char in key_value:
-        key_value[char] += 1
-    else:
-        key_value[char] =1
-r ={char:count for char,count in key_value.items() if count >1 }
-print(r)
+# a= 'ganesh grandhi'
+# key_value ={}
+# for char in a:
+#     if char in key_value:
+#         key_value[char] += 1
+#     else:
+#         key_value[char] =1
+# r ={char:count for char,count in key_value.items() if count >1 }
+# print(r)
 
+a='ganesh ankarao'
+d ={}
+for i in a:
+    if i in d:
+        d[i] += 1
+    else:
+        d[i] =1
+dic = {i:count for i,count in d.items() if count >1}
+print(dic)
 
 
 
